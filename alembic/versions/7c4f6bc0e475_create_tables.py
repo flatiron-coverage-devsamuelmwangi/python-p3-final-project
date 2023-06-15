9fd83ec9f8627e3b62d69ab1db5a7ac186f38423
devsamuelmwangi <hirewebappdeveloper@gmail.com>
2023-06-15 14:09:11 +0300

    feature(alembic): add db table creation and drop logic

--- alembic/versions/7c4f6bc0e475_create_tables.py
+++ alembic/versions/7c4f6bc0e475_create_tables.py
@@ -19,10 +19,32 @@ depends_on = None
 def upgrade():
     # Upgrade function to create tables
 
-    pass
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String)
+    )
+
+    op.create_table(
+        'products',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String),
+        sa.Column('price', sa.Integer)
+    )
+
+    op.create_table(
+        'reviews',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('star_rating', sa.Integer),
+        sa.Column('comment', sa.String),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('users.id')),
+        sa.Column('product_id', sa.Integer, sa.ForeignKey('products.id'))
+    )
 
 
 def downgrade():
     # Downgrade function to drop tables
 
-   pass
+    op.drop_table('reviews')
+    op.drop_table('products')
+    op.drop_table('users')
